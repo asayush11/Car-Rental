d1158f7f92855ffa99a461f346b149a4a47d94b8
Ayush Sharma <asayush35@gmail.com>
2025-04-22 16:26:31 +0530

    Added Booking Status

--- src/CardPayment.java
+++ src/CardPayment.java
@@ -4,6 +4,5 @@ public class CardPayment implements ProcessPayment{
     @Override
     public void processPayment(double amount){
         System.out.println("The amount to be paid via card is: " + amount );
-        return;
     }
 }
